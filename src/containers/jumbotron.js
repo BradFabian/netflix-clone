5b2d976b23965c70c9a7bc8f2947a3543c5359b0
Brad Fabian <bradleofabian@gmail.com>
2021-01-23 18:23:27 -0500

    jumbotron

--- src/containers/jumbotron.js
+++ src/containers/jumbotron.js
@@ -3,6 +3,8 @@ import jumboData from '../fixtures/jumbo.json';
 import Jumbotron from '../components/jumbotron';
 
 export function JumbotronContainer() {
+
+   
      return (
         
         <Jumbotron.Container>
@@ -10,7 +12,8 @@ export function JumbotronContainer() {
             <Jumbotron key={data.id}>
             <Jumbotron.Title>{data.title}</Jumbotron.Title>
         <Jumbotron.SubTitle>{data.subTitle}</Jumbotron.SubTitle>
-        <Jumbotron.Image src={data.image} alt={data.alt} />
+       
+        <Jumbotron.Image src={`${data.image}`} alt={data.alt} />
         </Jumbotron>
         )}
         
