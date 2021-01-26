86f6a8c661ce9cd68da904e08125cca40e208d56
Brad Fabian <bradleofabian@gmail.com>
2021-01-26 09:54:05 -0500

    added faqs structure

---
+++ src/components/accordion/styles/accordion.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const Container = styled.div``;
+
+export const Title = styled.p``;
+export const Header= styled.h1``;
+export const Body = styled.div``;
